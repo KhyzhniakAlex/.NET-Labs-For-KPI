ac86746b3197dd5e8857e2b400478df5824dbd5e
KhyzhniakAlex <sasha.khyzhniak@gmail.com>
2019-06-03 22:50:36 +0300

    1-4 labs work without deserialization

--- AcademicPerformanceUI/AcademicPerformanceUI/ViewModels/CarInOrderViewModel.cs
+++ AcademicPerformanceUI/AcademicPerformanceUI/ViewModels/CarInOrderViewModel.cs
@@ -8,7 +8,7 @@ namespace AcademicPerformanceUI.ViewModels
     public class CarInOrderViewModel : BaseViewModel<CarInOrder>
     {
         public ObservableCollection<Guid> CarIds { get; set; }
-        public ObservableCollection<Guid> OrdersIds { get; set; }
+        public ObservableCollection<Guid> OrderIds { get; set; }
 
         public CarInOrderViewModel()
         {
@@ -19,7 +19,7 @@ namespace AcademicPerformanceUI.ViewModels
         public override void LoadConnectedData()
         {
             CarIds = new ObservableCollection<Guid>(UnitOfWork.CarRepository.GetAllEntitiesAsync().Result.Select(o => o.Id));
-            OrdersIds = new ObservableCollection<Guid>(UnitOfWork.OrderRepository.GetAllEntitiesAsync().Result.Select(o => o.Id));
+            OrderIds = new ObservableCollection<Guid>(UnitOfWork.OrderRepository.GetAllEntitiesAsync().Result.Select(o => o.Id));
         }
     }
 }
